92fb56b725e9048af36800c6370ca13af6d3a595
chakyubeom <ckrbqja@gmail.com>
2023-01-08 12:53:44 +0900

    중복문자제거

---
+++ src/중복문자제거/Main.java
@@ -0,0 +1,23 @@
+package 중복문자제거;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main.solution(new Scanner(System.in).next());
+    }
+
+    private static void solution(String str) {
+        String answer = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (i == str.indexOf(ch)) {
+                answer += str.charAt(i);
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+}
